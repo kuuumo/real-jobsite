74694524fd3488eb8b79dec01a6d8f37db634e7f
kuuumo <0418ya@gmail.com>
2019-04-22 22:24:22 +0900

    finish validation,password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_224358) do
+ActiveRecord::Schema.define(version: 2019_04_22_122946) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
