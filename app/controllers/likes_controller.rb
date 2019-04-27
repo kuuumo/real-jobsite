673c1402e35f8cd1676ad0f7cedb4ef6949df6f5
kuuumo <0418ya@gmail.com>
2019-04-27 16:39:56 +0900

    unique

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,12 @@
 class LikesController < ApplicationController
 
   def create
-    @job = Job.find(params[:job_id])
+    @job = Job.find(params[:id])
     @job.likes.create(user_id: current_user.id)
   end
 
   def destroy
-    Like.find(user_id: current_user, job_id: params[:job_id]).destroy
+    Like.find(user_id: current_user, job_id: params[:id]).destroy
   end
 
 end
