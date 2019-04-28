eed47910e74fcc7071721b06ffa6a7fbff0f0dd4
kuuumo <0418ya@gmail.com>
2019-04-28 19:54:52 +0900

    finish route fix path name

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  
+
   root   'static_pages#home'
   get    '/signup',      to: 'users#new'
   post   '/signup',      to: 'users#create'
   get    '/login',       to: 'sessions#new'
   post   '/login',       to: 'sessions#create'
   delete '/logout',      to: 'sessions#destory'
-  get    '/postjob',     to: 'jobs#new'
-  post   '/postjob',     to: 'jobs#create'
-
   resources :users, shallow: true do
     resources :jobs
     resources :likes, only: [:create, :destroy, :index]
