419d5d67329d15640767266adfc97f4b62c6a2bb
kuuumo <0418ya@gmail.com>
2019-05-02 06:13:55 +0900

    final routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root   'static_pages#home'
   get    '/signup',      to: 'users#new'
   post   '/signup',      to: 'users#create'
-  get    '/login',       to: 'sessions#new'
-  post   '/login',       to: 'sessions#create'
-  delete '/logout',      to: 'sessions#destory'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, shallow: true do
     resources :jobs
     resources :likes, only: [:create, :destroy, :index]
